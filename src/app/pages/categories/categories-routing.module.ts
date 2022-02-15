bb803282daa07847c885cf5a7f75c943231fb42d
Alexandre Gonçalves <alexandre.goncalves33@outlook.com>
2022-02-15 19:38:57 -0300

    adding navbar

---
+++ src/app/pages/categories/categories-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CategoryFormComponent } from './category-form/category-form.component';
+import { CategoryListComponent } from './category-list/category-list.component';
+
+const routes: Routes = [
+  {path:'', component: CategoryListComponent},
+  {path:':id', component: CategoryFormComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CategoriesRoutingModule { }
