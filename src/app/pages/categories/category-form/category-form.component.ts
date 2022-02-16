731bffbbf2541e347e3eddc20236bca3cfa09b46
Alexandre Gonçalves <alexandre.goncalves33@outlook.com>
2022-02-16 17:04:09 -0300

    improving CategoryFormComponent

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -35,6 +35,15 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     this.setPageTitle();
   }
 
+  submitForm() {
+    this.submittingForm = true;
+
+    if (this.currentAction == 'new')
+      this.createCategory();
+    else
+      this.updateCategory();
+  }
+
   //PRIVATE METHODS
   private setCurrentAction() {
     if (this.route.snapshot.url[0].path == 'new') this.currentAction = 'new'
@@ -64,4 +73,35 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
       this.pageTitle = 'Editando Categoria: ' + categoryName
     }
   }
+
+  private createCategory() {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+    this.categoryService.create(category).subscribe(category => this.actionsForSuccess(category), error => this.actionsForError(error));
+  }
+
+  private updateCategory() {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+    this.categoryService.update(category).subscribe(category => this.actionsForSuccess(category), error => this.actionsForError(error));
+  }
+
+
+  private actionsForSuccess(category: Category) {
+    toastr.success("Solicitação processada com sucesso!")
+
+    this.router.navigateByUrl('categories', { skipLocationChange: true }).then(
+      () => this.router.navigate(['categories', category.id, 'edit'])
+    )
+  }
+
+  private actionsForError(error) {
+    toastr.error("Ocorreu um erro ao processar sua solicitação!");
+
+    this.submittingForm = false;
+
+    if (error.status === 422)
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    else
+      this.serverErrorMessages = ["Falha na comunicação com o servidor. Por favor, tente mais tarde."]
+  }
+
 }
