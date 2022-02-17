9f21f578b9525bc3d6d539aed696a90ffd9bfa8d
Alexandre Gonçalves <alexandre.goncalves33@outlook.com>
2022-02-17 11:51:48 -0300

    adding association between Entry and Category

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -6,6 +6,8 @@ import { map, catchError, flatMap } from "rxjs/operators";
 
 import { Entry } from "./entry.model";
 
+import { CategoryService } from './../../categories/shared/category.service';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,7 +15,7 @@ export class EntryService {
 
   private apiPath: string = 'api/entries';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private categoryService: CategoryService) { }
 
   getAll(): Observable<Entry[]> {
     return this.http.get(this.apiPath).pipe(catchError(this.handleError), map(this.jsonDataToEntries))
@@ -25,12 +27,23 @@ export class EntryService {
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.http.post(this.apiPath, entry).pipe(catchError(this.handleError), map(this.jsonDataToEntry))
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category
+        return this.http.post(this.apiPath, entry).pipe(catchError(this.handleError), map(this.jsonDataToEntry))
+      })
+    )
   }
 
   update(entry: Entry): Observable<Entry> {
     const url = `${this.apiPath}/${entry.id}`;
-    return this.http.put(url, entry).pipe(catchError(this.handleError), map(() => entry))
+
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category =>{
+        entry.category = category
+        return this.http.put(url, entry).pipe(catchError(this.handleError), map(() => entry))
+      })
+    ) 
   }
 
   delete(id: number): Observable<any> {
@@ -44,7 +57,7 @@ export class EntryService {
   private jsonDataToEntries(jsonData: any[]): Entry[] {
     const entries: Entry[] = [];
     jsonData.forEach(element => {
-      const entry =  Object.assign(new Entry(), element)
+      const entry = Object.assign(new Entry(), element)
       entries.push(entry)
     })
     return entries;
